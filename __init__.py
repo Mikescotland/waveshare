7036a7c200bc692d3b8a646099d7ded643505783
Mikescotland <52081323+Mikescotland@users.noreply.github.com>
2026-04-25 00:29:46 +0100

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,29 @@
+import esphome.codegen as cg
+import esphome.config_validation as cv
+from esphome.components import i2c
+from esphome.const import CONF_ID
+
+DEPENDENCIES = ["i2c"]
+
+waveshare_ns = cg.esphome_ns.namespace("waveshare_4b_lcd_init")
+Waveshare4BLcdInit = waveshare_ns.class_(
+    "Waveshare4BLcdInit",
+    cg.Component,
+    i2c.I2CDevice,
+)
+
+CONFIG_SCHEMA = (
+    cv.Schema(
+        {
+            cv.GenerateID(): cv.declare_id(Waveshare4BLcdInit),
+        }
+    )
+    .extend(cv.COMPONENT_SCHEMA)
+    .extend(i2c.i2c_device_schema(0x20))
+)
+
+
+async def to_code(config):
+    var = cg.new_Pvariable(config[CONF_ID])
+    await cg.register_component(var, config)
+    await i2c.register_i2c_device(var, config)
