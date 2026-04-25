b941b7c15f4240a3f092515cc8cbcca1e1f5d8de
Mikescotland <52081323+Mikescotland@users.noreply.github.com>
2026-04-25 09:56:12 +0100

    Update waveshare_4b_backlight_output.h

--- components/waveshare_4b_backlight/waveshare_4b_backlight_output.h
+++ components/waveshare_4b_backlight/waveshare_4b_backlight_output.h
@@ -4,8 +4,10 @@
 #include "esphome/core/log.h"
 #include "esphome/components/output/float_output.h"
 
+#include "driver/gpio.h"
 #include "driver/ledc.h"
 #include "esp_err.h"
+#include "esp_idf_version.h"
 
 namespace esphome {
 namespace waveshare_4b_backlight {
@@ -17,9 +19,12 @@ class Waveshare4BBacklightOutput : public Component, public output::FloatOutput 
 
  protected:
   void write_state(float state) override;
+  void configure_channel_(uint32_t initial_duty);
 
   static constexpr gpio_num_t BACKLIGHT_GPIO = GPIO_NUM_4;
 
+  // Jak w Waveshare / ESP-Brookesia BSP:
+  // GPIO4, LEDC_LOW_SPEED_MODE, timer 1, channel 1, 10-bit, 5000 Hz.
   static constexpr ledc_mode_t LEDC_MODE = LEDC_LOW_SPEED_MODE;
   static constexpr ledc_timer_t LEDC_TIMER = LEDC_TIMER_1;
   static constexpr ledc_channel_t LEDC_CHANNEL = LEDC_CHANNEL_1;
@@ -28,6 +33,7 @@ class Waveshare4BBacklightOutput : public Component, public output::FloatOutput 
   static constexpr ledc_timer_bit_t LEDC_RESOLUTION = LEDC_TIMER_10_BIT;
   static constexpr uint32_t LEDC_MAX_DUTY = 1023;
 
+  bool channel_configured_{false};
   float last_state_{1.0f};
 };
 
